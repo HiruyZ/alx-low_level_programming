d2693e36d33fd8e79eea55bc251909f449639b68
HiruyZ <hiruyz@gmail.com>
2023-03-21 13:19:35 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - find the absolute value
+ * @x: the value to be checked
+ * Return: Absolute value on x
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	return (-x);
+}
