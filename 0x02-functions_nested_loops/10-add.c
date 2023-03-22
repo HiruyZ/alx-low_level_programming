2c5eec28959837cde959a0680c5ae6bc3145edee
HiruyZ <hiruyz@gmail.com>
2023-03-21 19:51:21 -0700

    sum of a and b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of the two
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
