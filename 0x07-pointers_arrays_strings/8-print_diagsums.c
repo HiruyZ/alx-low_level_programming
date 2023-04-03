04c93ef977f320e44cf6f1795e73f015c61c2932
HiruyZ <hiruyz@gmail.com>
2023-04-03 12:27:48 -0700

    Diagonal sum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - display sum of the two diagonals
+ *@a: first value
+ *@size: the size
+ */
+void print_diagsums(int *a, int size)
+{
+	int x, y, suma_a = 0, suma_b = 0, a, b = 0;
+
+	a = size - 1;
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; y++)
+		{
+			if (x == j)
+				suma_a += a[b];
+			if (y == a)
+				suma_b += a[b];
+			l++;
+		}
+		a--;
+	}
+	printf("%i, %i\n", suma_a, suma_b);
+}
