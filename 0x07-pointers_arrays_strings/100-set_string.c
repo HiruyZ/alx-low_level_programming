cbed735964536b7c0a6ffca64e22954d368de36c
HiruyZ <hiruyz@gmail.com>
2023-04-03 13:15:55 -0700

    Pointer to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - set value of the pointer to a character
+ * @s: pointer in pointer
+ * @to: pointer to char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
