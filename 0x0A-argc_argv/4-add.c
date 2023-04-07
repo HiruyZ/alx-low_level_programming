a01dce052b8c263a7a7f0e5be565d084ec3ed200
HiruyZ <hiruyz@gmail.com>
2023-04-07 12:14:01 -0700

    add arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - adds two numbers
+ * @argc: argument count
+ * @argv: array of the arguments
+ * Return:0 on success and 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int sum;
+	int count;
+	int i;
+
+	count = 1;
+	sum = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (count < argc)
+	{
+		for (i = 0; argv[count][i] != '\0'; i++)
+		{
+			if (!(isdigit(argv[count][i])))
+			{
+				printf("Not a number\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[count]);
+		count++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
