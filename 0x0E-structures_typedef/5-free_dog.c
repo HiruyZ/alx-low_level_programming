d7d8310a0ddb25b1512333aab46bf821503e1d0c
HiruyZ <hiruyz@gmail.com>
2023-04-18 08:42:35 -0700

    Free dog memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees dog memory
+ * @d: The dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
