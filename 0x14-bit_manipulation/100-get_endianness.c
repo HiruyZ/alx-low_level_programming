ea7e2775b89ed4488838a966b34ac73f901c7b21
HiruyZ <hiruyz@gmail.com>
2023-05-04 12:26:39 -0700

    Task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - check endianness
+ *
+ * Return: 0 if big, 1 if little
+ */
+int get_endianness(void)
+{
+	unsigned int i;
+	char *c;
+
+	i = 1;
+	c = (char *) &i;
+
+	return ((int)*c);
+}
